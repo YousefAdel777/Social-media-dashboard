f218d570229a2d078b243e5325f00e9fd4803a84
Yuosef <www.yoyoadel@gmail.com>
2024-02-04 12:55:02 +0200

    accessibility optimization

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,16 @@ if (localStorage.theme === "light") {
     document.documentElement.classList.remove("dark");
 }
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", () => switchTheme())
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Tab") {
+        event.preventDefault();
+        switchTheme();
+    }
+});
+
+function switchTheme () {
     document.documentElement.classList.toggle("dark");
     if (document.documentElement.classList.contains("dark")) {
         localStorage.theme = "dark";
@@ -12,4 +21,4 @@ toggle.addEventListener("click", () => {
     else {
         localStorage.theme = "light";
     }
-});
+}
